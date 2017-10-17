156a10f39178300e2487773957213fe89f0dc8ed
Michael R. Bechtel <mikebec20@gmail.com>
2017-10-16 20:49:23 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Navbar from "./components/Navbar.js";
+import Container from "./components/Container.js";
+import Main from "./components/Main.js";
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Navbar
+        <Container
+          <Main
+
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
